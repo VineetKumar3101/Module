787fd319025096c8bdc9e123787cd13104353a08
Vineet Kumar <vineet.kumar_cs20@gla.ac.in>
2022-01-25 16:07:07 +0530

    updated

--- MyModule.py
+++ MyModule.py
@@ -3,4 +3,13 @@ def facti(n):
     for i in range(1,n+1):
         fact=fact*i
     return fact
-
+def s(a,*b):
+    res=a
+    for j in b:
+        res=res+j
+    return res
+def E(d):
+    if((d%2)==0):
+        return "Even"
+    else:
+        return "Odd"
